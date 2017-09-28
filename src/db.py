252e258cd93f778616673871b3adb6a2d9932d2a
ameyazing <ameya.nayak@gmail.com>
2017-09-28 17:37:55 +0530

    completed code of db insertion. Not completely tested yet.

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import pymongo
 import logging
 
 #TODO: Replace this global variable with state maintained in Actor-model / gen_server
@@ -31,27 +32,64 @@ def db_transact(db_func, pObj):
 #criteria: JSON object which will be the criteria for which doc to update
 #upsert: Create new doc if criteria fails
 #obj: JSON object containing all keys who's values need to be udpated
-#Return value: 
-def db_update_one(ISIN, obj, criteria, upsert=True):
-	
+#Return value: _id of the updated document if successful, else None
+def db_modify_one(pISIN, obj, criteria, upsert=True, replace=False):
+	db = establish_conn()
+	if db == None:
+		return None
+	update_result = None
+	try:
+		if replace == True:
+			update_result = db.stocks.replace_one({"isin":pISIN},{"$set":obj}, upsert=upsert)
+		else:
+			update_result = db.stocks.update_one({"isin":pISIN},{"$set":obj}, upsert=upsert)
+	except pymongo.errors.ConnectionFailure:
+		logging.error("DB Connection Failure")
+	except pymongo.errors.ConfigurationError:
+		logging.error("DB Configuration Error")
+	except pymongo.errors.CollectionInvalid:
+		logging.error("DB Collection Invalid")
+	except pymongo.errors.InvalidName:
+		logging.error("DB Invalid Name")
+	except pymongo.errors.InvalidOperation:
+		logging.error("DB Invalid Operation")
+	except pymongo.errors.InvalidURI:
+		logging.error("DB Invalid URI")
+	except pymongo.errors.NetworkTimeout:
+		logging.error("DB Network Timeout")
+	except pymongo.errors.WriteError:
+		#logging.error("DB Write Error (error:{}, code:{}, details:{})".format(e.error, e.code, e.details))
+		logging.error("DB Write Error")
+	except:
+		logging.error("DB Unhandled error")
+	if update_result.acknowledged == True:
+		return update_result.upserted_id
+	else:
+		logging.warning("Upsert Failed for ISIN: {}".format(pISIN))
+		return None
+
 def db_write_company(ISIN, obj, replace = False):
 	db = establish_conn()
 	if db == None:
 		return None
-	stock = db_transact(db.stocks.find_one, {"isin":ISIN})
-	if stock == None:
+	updated_id = db_modify_one(ISIN, obj, {"isin":ISIN}, upsert=True, replace=replace)
+	if updated_id == None:
 		return None
-	db_transact(db.stocks.insert_one, obj)
 
-if __name__ == "__main__":
+def test_function():
 	stock1 = {
 		'company_name': 'Maruti Suzuki India',
-		'main_page_url': 'http://www.moneycontrol.com/india/stockpricequote/autocarsjeeps/marutisuzukiindia/MS24',
+#		'main_page_url': 'http://www.moneycontrol.com/india/stockpricequote/autocarsjeeps/marutisuzukiindia/MS24',
 		'company_id': 'MU01',
-		'sector_name': 'Automotive',
-		'sector_id': 'AU'
+#		'sector_name': 'Automotive',
+#		'sector_id': 'AU'
 	}
-	db = establish_conn()
-	print(db)
-	db.db_write_company("1234567890", stock1)
+	db_handle = establish_conn()
+	if db_handle == None:
+		print("Failed to establish connection")
+		return
+	db_write_company("1234567890", stock1, replace=True)
 
+if __name__ == "__main__":
+	print("Executing db __main__")
+	test_function()
