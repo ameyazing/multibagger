089d01714a1ac697a2d30aab8457d56de9ba7167
ameyazing <ameya.nayak@gmail.com>
2017-10-04 17:48:43 +0000

    Module for parsing main_page and extracting URLs necessary for crawling

---
+++ src/mc_extract_urls.py
@@ -0,0 +1,7 @@
+import logging
+
+#page: main page html.
+#Sample main page URL: 'http://www.moneycontrol.com/india/stockpricequote/autocarsjeeps/marutisuzukiindia/MS24'
+#Return value: JSON object with all required URLs; None if unsuccessful
+def extract_main_page_urls(page):
+	return None
