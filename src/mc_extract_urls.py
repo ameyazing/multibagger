632a48fb205a0457e8302ff40e4266b4084b8a37
ameyazing <ameya.nayak@gmail.com>
2017-10-05 16:01:34 +0530

    Added HTML parser class

--- src/mc_extract_urls.py
+++ src/mc_extract_urls.py
@@ -1,4 +1,18 @@
 import logging
+from html.parser import HTMLParser
+
+class URLExtractor(HTMLParser):
+	def handle_starttag(self, tag, attrs):
+		print("Encountered a start tag: {} and attrs: {}".format(tag, attrs))
+
+	def handle_endtag(self, tag, attrs):
+		print("Encountered end tag: {} and attrs".format(tag, attrs))
+
+	def handle_startendtag(self, tag, attrs):
+		print("Encountered startend tag: {} and attrs".format(tag, attrs)
+
+	def handle_data(self, tag, attrs):
+		print("Encountered end tag: {} and attrs".format(tag, attrs))
 
 #page: main page html.
 #Sample main page URL: 'http://www.moneycontrol.com/india/stockpricequote/autocarsjeeps/marutisuzukiindia/MS24'
